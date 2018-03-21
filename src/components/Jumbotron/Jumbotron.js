fd826f79fd70576445b3d92ca173b95c135341aa
hmalle <hmalle90ak@gmail.com>
2018-03-20 20:40:04 -0700

    getting ahead of myself

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = props =>(
+  <div className="jumbotron bg-dark">
+    <h2 className="elem">Recollector</h2>
+    <p className="elem"> Let check how good is your recollection </p>
+    <p className="elem"> Click on an image to get started, Dont click on the same image twice</p>
+  </div>
+);
+
+export default Jumbotron;
