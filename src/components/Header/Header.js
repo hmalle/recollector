c5168f9449751cce7c578c5d6cbfacea1772d07a
hmalle <hmalle90ak@gmail.com>
2018-03-21 19:03:38 -0700

    edited the header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,10 @@ import "./Header.css";
 
 const Title = props => 
   <div className="fixed-top bg-dark">
-    <h1 className="float-left header">Total Recall</h1>
-    <p className="float-right header">{props.children}</p>
+    <div className="container">
+      <h1 className="float-left header">Total Recall</h1>
+      <p className="float-right header">{props.children}</p>
+    </div>
   </div>
 
 export default Title;
